83c4dc5ec09301b0632fdf84819fceb6e344b07b
EugenKoptikov <evgenkoptikov100500@gmail.com>
2022-07-12 00:44:40 +0300

    uwu

---
+++ Fifth seminar/Test/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите количество элементов массива ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[a];
+
+double Raznicha(double[] collection)
+{
+   
+    Random rand = new Random();
+    double razn = 0;
+    Console.WriteLine("Массив чисел: ");
+    for (int i = 0; i<collection.Length; i++)
+    {
+        array[i] = rand.Next(1, 10000);
+        Console.Write(" " + array[i]); 
+    }   
+    double min = 100000;
+    double max = 0;
+     for (int i = 0; i<array.Length; i++)
+     {
+        if (collection[i] > max)
+        {
+            max = collection[i];
+        }
+        else if (collection[i] < min)
+        {
+            min = collection[i];
+        }
+        
+        razn = max - min;
+     }
+     return razn;
+}
+
+
+double result = Raznicha(array);
+Console.WriteLine("");
+Console.WriteLine("Разница между максимумом и минимумом  = " + result);
